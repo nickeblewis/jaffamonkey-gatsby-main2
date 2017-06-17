7d8d48e96fb77278095a5ebb3bd9a39d40a1e105
Eddy Hernandez <edward.d.hernandez@gmail.com>
2017-06-16 23:10:22 -0700

    add circle and deploy scripts

---
+++ scripts/deploy.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+set -e
+
+CURRENT_BRANCH="$(git symbolic-ref --short -q HEAD)"
+BUILD_DIR="public/"
+
+success() {
+  echo -e "\033[32;1m$1"
+}
+
+error() {
+  echo -e "\033[31;1m$1"
+}
+
+if [ -z "$S3PATH" ]; then
+  echo "S3PATH not set, defaulting to current branch ${CURRENT_BRANCH}"
+  S3PATH=$CURRENT_BRANCH
+fi
+
+S3PATH=${S3PATH} npm run build
+
+if [ ! -d $BUILD_DIR ]; then
+  error "Build directory does not exist. Stopping deploy < / 3"
+  exit 1
+fi
+
+if [ "$S3PATH" == "latest" ]; then
+  echo "backing up current site..."
+  echo "npm run s3:backup"
+fi
+
+echo "deploying site..."
+echo "S3PATH=${S3PATH} npm run s3:upload"
+
+success "pushed site to $S3PATH"
